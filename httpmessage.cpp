89d55280b4be864a306cb18cbc57b45b8028404c
hanqiu8 <hanqiu8@gmail.com>
2016-04-09 06:19:29 +0000

    httpmessage.cpp and httpmessage.h additions for set version

---
+++ httpmessage.cpp
@@ -0,0 +1,48 @@
+#include httpmessage.h
+#include <string>
+#include <iostream>
+#include <map>
+
+using namespace std;
+
+virtual void HttpMessage::decodeFirstLine(string line) = 0;
+
+string HttpMessage::getVersion(){
+  return m_version;
+}
+
+string HttpMessage::setVersion(string ver){
+  m_version = ver;
+}
+
+void HttpMessage::setHeader(string key, string value)
+{
+  m_headers[key] = value;
+  return;
+}
+
+string HttpMessage::getHeader(string key)
+{
+  return m_headers[key];
+}
+
+void HttpMessage::decodeHeaderLine(string line)
+//May need error checking if header line does not end with carriage return
+{
+  string key, value;
+  int i=0;
+  while(line[i]!= ':')
+    {
+      key += line[i];
+      i++;
+    }
+  i++;
+  while(line[i] == ' ')
+    i++;
+  while(line[i] != '\r' && line[i+1] != '\n')
+    {
+      value += line[i];
+    }
+  setHeader(key, value);
+  return;
+}
