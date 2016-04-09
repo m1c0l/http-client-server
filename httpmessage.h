4c85fc3b1fed595743a8a82132128e21f2c7f79e
hanqiu8 <hanqiu8@gmail.com>
2016-04-09 06:04:51 +0000

    fixed include errors

--- httpmessage.h
+++ httpmessage.h
@@ -1,27 +1,25 @@
 #ifndef HTTPMESSAGE
 #define HTTPMESSAGE
 #include <string>
-#include <iostream>
-#include <stdlib>
-
+#include <cstdlib>
+#include <map>
 
 using namespace std;
 
 class HttpMessage{
  public:
-  float  m_version;
-  map<string, string> m_headers;
-  string m_payload;
-  
-float getversion();
-void setHeader(string key, string value);
-string getHeader(string key);
-void decodeHeaderLine(string line);
-void setPayLoad(string blob);
-string getPayload();
+  //HttpMessage();
+  float getversion();
+  void setHeader(string key, string value);
+  string getHeader(string key);
+  void decodeHeaderLine(string line);
+  void setPayLoad(string blob);
+  string getPayload();
 
  private:
-
+  float m_version;
+  map<string, string> m_headers;
+  string m_payload;
 
    ;
 };
