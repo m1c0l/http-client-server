c9f454db29a847796fe11b623b6ee3fdc5803754
Michael Li <michael.li11702@gmail.com>
2016-04-08 22:30:07 -0700

    Reformat, add processing for default cmd line args

--- web-server.cpp
+++ web-server.cpp
@@ -12,91 +12,91 @@
 
 using namespace std;
 
-int
-main(int argc, char **argv)
-{
-	if (argc != 4) {
+int main(int argc, char **argv) {
+	if (argc > 4) {
 		cerr << "usage: web-server [hostname] [port] [file-dir]" << '\n';
 		return 1;
 	}
 
-	string hostname = argv[1];
-	string port = argv[2];
-	string filedir = argv[3];
+	string hostname, port, filedir;
+
+	// check argc and set default parameters if needed
+	hostname = argc < 2 ? "localhost" : argv[1];
+	port = argc < 3 ? "4000" : argv[2];
+	filedir = argc < 4 ? "." : argv[3];
+
+	// create a socket using TCP IP
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// allow others to reuse the address
+	int yes = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		perror("setsockopt");
+		return 1;
+	}
+
+	// bind address to socket
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(40000);     // short, network byte order
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+
+	if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+		perror("bind");
+		return 2;
+	}
+
+	// set socket to listen status
+	if (listen(sockfd, 1) == -1) {
+		perror("listen");
+		return 3;
+	}
+
+	// accept a new connection
+	struct sockaddr_in clientAddr;
+	socklen_t clientAddrSize = sizeof(clientAddr);
+	int clientSockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
+
+	if (clientSockfd == -1) {
+		perror("accept");
+		return 4;
+	}
+
+	char ipstr[INET_ADDRSTRLEN] = {'\0'};
+	inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
+	std::cout << "Accept a connection from: " << ipstr << ":" <<
+		ntohs(clientAddr.sin_port) << std::endl;
+
+	// read/write data from/into the connection
+	bool isEnd = false;
+	char buf[20] = {0};
+	std::stringstream ss;
+
+	while (!isEnd) {
+		memset(buf, '\0', sizeof(buf));
+
+		if (recv(clientSockfd, buf, 20, 0) == -1) {
+			perror("recv");
+			return 5;
+		}
+
+		ss << buf << std::endl;
+		std::cout << buf << std::endl;
 
 
-  // create a socket using TCP IP
-  int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (send(clientSockfd, buf, 20, 0) == -1) {
+			perror("send");
+			return 6;
+		}
 
-  // allow others to reuse the address
-  int yes = 1;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-    perror("setsockopt");
-    return 1;
-  }
+		if (ss.str() == "close\n")
+			break;
 
-  // bind address to socket
-  struct sockaddr_in addr;
-  addr.sin_family = AF_INET;
-  addr.sin_port = htons(40000);     // short, network byte order
-  addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+		ss.str("");
+	}
 
-  if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-    perror("bind");
-    return 2;
-  }
+	close(clientSockfd);
 
-  // set socket to listen status
-  if (listen(sockfd, 1) == -1) {
-    perror("listen");
-    return 3;
-  }
-
-  // accept a new connection
-  struct sockaddr_in clientAddr;
-  socklen_t clientAddrSize = sizeof(clientAddr);
-  int clientSockfd = accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize);
-
-  if (clientSockfd == -1) {
-    perror("accept");
-    return 4;
-  }
-
-  char ipstr[INET_ADDRSTRLEN] = {'\0'};
-  inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-  std::cout << "Accept a connection from: " << ipstr << ":" <<
-    ntohs(clientAddr.sin_port) << std::endl;
-
-  // read/write data from/into the connection
-  bool isEnd = false;
-  char buf[20] = {0};
-  std::stringstream ss;
-
-  while (!isEnd) {
-    memset(buf, '\0', sizeof(buf));
-
-    if (recv(clientSockfd, buf, 20, 0) == -1) {
-      perror("recv");
-      return 5;
-    }
-
-    ss << buf << std::endl;
-    std::cout << buf << std::endl;
-
-
-    if (send(clientSockfd, buf, 20, 0) == -1) {
-      perror("send");
-      return 6;
-    }
-
-    if (ss.str() == "close\n")
-      break;
-
-    ss.str("");
-  }
-
-  close(clientSockfd);
-
-  return 0;
+	return 0;
 }
