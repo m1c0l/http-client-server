e4d3ad70d20b36d41333261d4afd457955a4ee76
hanqiu8 <hanqiu8@gmail.com>
2016-04-09 05:41:49 +0000

    Header for httpmessage

---
+++ httpmessage.h
@@ -0,0 +1,31 @@
+#ifndef HTTPMESSAGE
+#def HTTPMESSAGE
+#include <string>
+#include <iostream>
+#include <stdlib>
+
+
+using namespace std;
+
+class HttpMessage{
+ public:
+  float  m_version;
+  map<string, string> m_headers;
+  string m_payload;
+  
+float getversion();
+void setHeader(string key, string value);
+string getHeader(string key);
+void decodeHeaderLine(string line);
+void setPayLoad(string blob);
+string getPayload();
+
+ private:
+
+
+   ;
+};
+
+
+
+#endif
