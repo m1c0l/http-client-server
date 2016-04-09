4e5ea6099f80434ddee4b2520bb9ff981f0206f7
Richard Sun <rsun@media.ucla.edu>
2016-04-08 23:10:13 -0700

    most of HttpResponse class

---
+++ HttpResponse.h
@@ -0,0 +1,23 @@
+#ifndef HTTPRESPONSE_H
+#define HTTPRESPONSE_H
+
+#include <string>
+#include "httpmessage.h"
+using namespace std;
+
+typedef unsigned short HttpStatus;
+
+class HttpResponse : public HttpMessage {
+private:
+	HttpStatus m_status;
+	string m_statusDescription;
+
+public:
+	virtual void decodeFirstLine(string line);
+	HttpStatus getStatus();
+	void setStatus(HttpStatus status);
+	string getDescription();
+	void setDescription(string description);
+};
+
+#endif
