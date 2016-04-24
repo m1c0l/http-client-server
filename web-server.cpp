446bbb7bd7cea36b0498bd3a8ce442e9e6cb4e51
Richard Sun <rsun@media.ucla.edu>
2016-04-23 23:56:13 -0700

    move thread stuff to a function

--- web-server.cpp
+++ web-server.cpp
@@ -18,63 +18,7 @@ const int  BUFFER_SIZE = 200;
 
 using namespace std;
 
-int main(int argc, char **argv) {
-	if (argc > 4) {
-		cerr << "usage: web-server [hostname] [port] [file-dir]" << '\n';
-		return 1;
-	}
-
-	string hostname, port, filedir;
-
-	// check argc and set default parameters if needed
-	hostname = argc < 2 ? "localhost" : argv[1];
-	port = argc < 3 ? "4000" : argv[2];
-	filedir = argc < 4 ? "." : argv[3];
-
-	// create a socket using TCP IP
-	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-	// allow others to reuse the address
-	int yes = 1;
-	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
-		perror("setsockopt");
-		return 1;
-	}
-
-	// bind address to socket
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(stoi(port));     // short, network byte order
-	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
-
-	if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
-		perror("bind");
-		return 2;
-	}
-
-	// set socket to listen status
-	if (listen(sockfd, 1) == -1) {
-		perror("listen");
-		return 3;
-	}
-
-	// accept a new connection
-	struct sockaddr_in clientAddr;
-	socklen_t clientAddrSize = sizeof(clientAddr);
-	int clientSockfd;
-	
-	//Start Multithreading Magic
-	while( clientSockfd= accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize)){
-
-	if (clientSockfd == -1) {
-		perror("accept");
-		return 4;
-	}
-
-	}
-
-	
+int join() {
 	char ipstr[INET_ADDRSTRLEN] = {'\0'};
 	inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
 	std::cout << "Accept a connection from: " << ipstr << ":" <<
@@ -176,6 +120,65 @@ int main(int argc, char **argv) {
 	send(clientSockfd, responseMessage.c_str(), responseMessage.size(), 0);
 
 	close(clientSockfd);
+}
+
+int main(int argc, char **argv) {
+	if (argc > 4) {
+		cerr << "usage: web-server [hostname] [port] [file-dir]" << '\n';
+		return 1;
+	}
+
+	string hostname, port, filedir;
+
+	// check argc and set default parameters if needed
+	hostname = argc < 2 ? "localhost" : argv[1];
+	port = argc < 3 ? "4000" : argv[2];
+	filedir = argc < 4 ? "." : argv[3];
+
+	// create a socket using TCP IP
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	// allow others to reuse the address
+	int yes = 1;
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+		perror("setsockopt");
+		return 1;
+	}
+
+	// bind address to socket
+	struct sockaddr_in addr;
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(stoi(port));     // short, network byte order
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
+
+	if (bind(sockfd, (struct sockaddr*)&addr, sizeof(addr)) == -1) {
+		perror("bind");
+		return 2;
+	}
+
+	// set socket to listen status
+	if (listen(sockfd, 1) == -1) {
+		perror("listen");
+		return 3;
+	}
+
+	// accept a new connection
+	struct sockaddr_in clientAddr;
+	socklen_t clientAddrSize = sizeof(clientAddr);
+	int clientSockfd;
+	
+	//Start Multithreading Magic
+	while( clientSockfd= accept(sockfd, (struct sockaddr*)&clientAddr, &clientAddrSize)){
+
+	if (clientSockfd == -1) {
+		perror("accept");
+		return 4;
+	}
+
+	}
+
+	
 
 	return 0;
 }
